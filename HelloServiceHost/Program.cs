491fa804ac7f47e700a38e4daf6aeceafb15a90c
[suyashjain27] <[er.suyashjain27@gmail.com]>
2020-04-05 11:16:10 +0530

    Web service Demo code.

---
+++ HelloServiceHost/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using HelloServiceHost;
+
+namespace HelloServiceHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ServiceHost host = new ServiceHost(typeof(WcfService1.Service1))) 
+            {
+                host.Open();
+                Console.WriteLine("Service has been started @ " + DateTime.Now);
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ WcfService1/IService1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfService1
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract(Name = "IService1")]
+    public interface IService1change
+    {
+
+        [OperationContract]
+        string GetMessage(string name);
+
+   
+    }
+    //[ServiceContract]
+    //public interface IService2
+    //{
+
+    //    [OperationContract]
+    //    string GetConfidentialMessage(string name);
+
+
+    //}
+    //[ServiceContract]
+    //public interface IService3
+    //{
+
+    //    [OperationContract]
+    //    string GetPersonalMessage(string name);
+
+
+    //}
+}
